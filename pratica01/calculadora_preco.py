0a34097370973c50ff8391d52d46405cd6ac5bc8
Hélio Camêlo <heliotdk@gmail.com>
2025-05-16 20:20:19 -0300

    Atualização na pasta pratica01

---
+++ pratica01/calculadora_preco.py
@@ -0,0 +1,9 @@
+nomeDoProduto = "Cadeira Infantil"
+
+precoUnitario = 12.40
+
+quantidadeProduto = 3
+
+precoTotal = quantidadeProduto * precoUnitario
+
+print(f"Produto: {nomeDoProduto}. Preço total do produto: R${precoTotal}")
---
+++ pratica01/soma.py
@@ -0,0 +1,6 @@
+numero1 = 12
+numero2 = 14
+
+resultado = numero1 + numero2
+
+print(f"O resultado da soma de {numero1} e {numero2} é igual a: {resultado}")
